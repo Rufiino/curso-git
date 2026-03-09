337dd541f712a557367d6f0d68c9fe59c500c46c
Rafael Rufino <Rafiinha159@outlook.com>
2026-03-09 14:50:34 -0300

    Add goblin knife script

---
+++ goblin_knife.lua
@@ -0,0 +1,36 @@
+function init()
+
+  g_game.loadMonsters(
+    {
+      {
+        spawnName = "goblin_knife",
+        name = tr2("Goblin"),
+        race = "blood",
+        tags = { "goblin", "goblin_knife", "goblinmob", "goblins" },
+        lookType = 58,
+        groupId = "goblins",
+
+        speed = 200,
+        healthFactor = 1,
+        weakFromPower = 680,
+
+        colorMapBaseName = "goblin",
+        randomColorMapChance = 70,
+
+        targetDistance = 1,
+
+        walkSounds = 'regular',
+
+        deathAnimation = 2,
+
+        loot = {
+          { itemId = ITEMS.goblinTooth },
+        },
+
+        attacks = {
+          { target = true, cooldown = 2000, chance = 100, subname = "basic", range = 1, script = "goblins/goblin_knife_basic.lua", attackPercent = 10 }
+        }
+      },
+    }
+  )
+end
