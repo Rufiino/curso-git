7320d0f93c83faa9a3982ff01f7fe5fb33746c95
Rafael Rufino <Rafiinha159@outlook.com>
2026-03-09 15:35:10 -0300

    Add orc

---
+++ orc.lua
@@ -0,0 +1,35 @@
+function init()
+
+  g_game.loadMonsters(
+    {
+      {
+        spawnName = "orc",
+        name = tr2("Orc"),
+        race = "blood",
+        tags = { "orc" },
+        lookType = 186,
+        groupId = "orcs",
+
+        speed = 260,
+        healthFactor = 1.35,
+        strengthMultiplier = 1.5,
+        weakFromPower = 1140,
+        minOpenWorldPower = 680,
+
+        targetDistance = 1,
+
+        walkSounds = 'regular',
+
+        deathAnimation = 2,
+
+        loot = {
+          { itemId = ITEMS.orcFang },
+        },
+
+        attacks = {
+          { target = true, cooldown = 2500, chance = 100, subname = "basic", range = 1, outfit = 187, script = "orcs/orc_basic.lua", attackPercent = 16 },
+        }
+      },
+    }
+  )
+end
